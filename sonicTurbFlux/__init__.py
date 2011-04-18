dd5cc6464bb96b704d98602b48dc4926a23b8668
Andrey Simurzin <asimurzin@gmail.com>
2011-04-18 09:44:39 +0400

    to simplify condidtions and add entry_point

--- sonicTurbFlux/__init__.py
+++ sonicTurbFlux/__init__.py
@@ -27,60 +27,38 @@
 import sys, os
 from Foam import FOAM_VERSION, FOAM_BRANCH_VERSION, FOAM_REF_VERSION
 if FOAM_VERSION( "<=", "010401" ):
-   if __name__ == "__main__" :
-      argv = sys.argv
-      if len( argv ) > 1 and argv[ 1 ] == "-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'local', 'r1.4.1-dev', 'sonicTurbFoam' )
-         argv = [ __file__, test_dir, 'prism' ]
-         pass
-      from Foam.applications.solvers.compressible.r1_4_1_dev.sonicTurbFoam import main_standalone
-      os._exit( main_standalone( len( argv ), argv ) )
-   else:
-      from Foam.applications.solvers.compressible.r1_4_1_dev.sonicTurbFoam import *
-      pass
-   pass
+    from sonicTurbFlux.r1_4_1_dev import *
+    pass
 
 
 #--------------------------------------------------------------------------------------
 if FOAM_REF_VERSION( "==", "010500" ):
-   if __name__ == "__main__" :
-      argv = sys.argv
-      if len( argv ) > 1 and argv[ 1 ] == "-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'local', 'r1.5', 'sonicTurbFoam', 'prism' )
-         argv = [ __file__, "-case", test_dir ]
-         pass
-      from Foam.applications.solvers.compressible.r1_5.sonicTurbFoam import main_standalone
-      os._exit( main_standalone( len( argv ), argv ) )
-      pass
-   else:
-      from Foam.applications.solvers.compressible.r1_5.sonicTurbFoam import *
-      pass
-   pass
+    from sonicTurbFlux.r1_5 import *
+    pass
 
 
 #--------------------------------------------------------------------------------------
 if FOAM_BRANCH_VERSION( "dev", "==", "010500" ):
-   if __name__ == "__main__" :
-      argv = sys.argv
-      if len( argv ) > 1 and argv[ 1 ] == "-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases' )
-         argv = [ __file__, "-case", test_dir ]
-         pass
-      from Foam.applications.solvers.compressible.r1_5_dev.sonicTurbFoam import main_standalone
-      os._exit( main_standalone( len( argv ), argv ) )
-      pass
-   else:
-      from Foam.applications.solvers.compressible.r1_5_dev.sonicTurbFoam import *
-      pass
-   pass
+    from sonicTurbFlux.r1_5_dev import *
+    pass
 
 
 #--------------------------------------------------------------------------------------
-if FOAM_VERSION( ">", "010500" ):
-   from Foam.OpenFOAM import ext_Info
-   ext_Info() << "\n\n To use this solver it is necessary to SWIG OpenFOAM-1.4.X or 1.5(dev)\n"
-   pass
+def entry_point():
+    try:
+       import sys; argv = sys.argv
+       return main_standalone( len( argv ), argv )
+    except NameError:
+       print
+       print "There is no implementation of the current OpenFOAM version"
+       print
+       pass
 
+
+#--------------------------------------------------------------------------------------
+if __name__ == "__main__" :
+    entry_point()
+    pass
+    
+    
+#--------------------------------------------------------------------------------------
